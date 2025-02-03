cf1278d61a83ec7f3cc71b74dcf01d9835c3f5cf
Mike Qin <1mike12@gmail.com>
2025-02-03 17:40:33 -0600

    add windows test

---
+++ src/windows/getWindowsPorts.test.ts
@@ -0,0 +1,13 @@
+import * as os from 'os';
+import assert from 'assert';
+import test from 'node:test';
+import {getWindowsPorts} from "./getWindowsPorts";
+
+test('getMacPorts', {skip: os.platform() !== 'win32'}, async () => {
+    const ports = await getWindowsPorts();
+    assert(ports instanceof Set, 'Expected ports to be an instance of Set');
+    for (let port of Array.from(ports)) {
+        assert(typeof port === 'number', 'Expected port to be a number');
+        assert(port > 0, 'Expected port to be above 0');
+    }
+});
