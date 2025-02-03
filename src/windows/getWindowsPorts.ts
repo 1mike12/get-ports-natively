fe1a1c29d97dd611910725ccc103e528ea0d06e6
Mike Qin <1mike12@gmail.com>
2025-02-03 17:44:35 -0600

    change command to use powershell

--- src/windows/getWindowsPorts.ts
+++ src/windows/getWindowsPorts.ts
@@ -1,9 +1,9 @@
 import {execAndGetOutput} from "../execAndGetOutput";
 
 export async function getWindowsPorts() {
-    const command = `netstat -an | Select-String "LISTENING" | ForEach-Object { (($_.ToString() -split "\\s+")[1] -replace '^\\[|\\]$', '') -split ":" | Select-Object -Last 1 } | Sort-Object -Unique`
+    const command = `powershell -Command "netstat -an | Select-String 'LISTENING' | ForEach-Object { (($_.ToString() -split '\\s+')[1] -replace '^\\[|\\]$', '') -split ':' | Select-Object -Last 1 } | Sort-Object -Unique"`;
     const result = await execAndGetOutput(command);
-    const set = new Set<number>()
+    const set = new Set<number>();
     for (let line of result.split('\n')) {
         const port = parseInt(line.trim());
         if (port) {
